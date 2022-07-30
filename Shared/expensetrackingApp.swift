c386135a70088bd2586b8ffb97aef8d6ef390df0
mustafa <msmasud97@gmail.com>
2022-07-30 19:39:34 -0400

    Initial Commit

---
+++ Shared/expensetrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  expensetrackingApp.swift
+//  Shared
+//
+//  Created by Mustafa on 7/30/22.
+//
+
+import SwiftUI
+
+@main
+struct expensetrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
