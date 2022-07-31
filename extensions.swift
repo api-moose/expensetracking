34c50b183a83c775637e0e063d862d85b0d2d1e1
mustafa <msmasud97@gmail.com>
2022-07-30 20:33:55 -0400

    jul 30 2022 committed

---
+++ extensions.swift
@@ -0,0 +1,15 @@
+//
+//  extensions.swift
+//  expensetracking
+//
+//  Created by Mustafa on 7/30/22.
+//
+
+import Foundation
+import SwiftUI
+extension Color {
+    static let background = Color("background")
+    static let icon = Color("icon")
+    static let text = Color("text")
+    static let systemBackground = Color(uiColor: .systemBackground)
+}
